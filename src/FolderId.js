0275c1c9c668238a1f0e3063b350572b915aabcd
Michael Qassis <mikeqassis@gmail.com>
2020-04-07 21:30:12 -0700

    before css

--- src/FolderId.js
+++ src/FolderId.js
@@ -1,16 +1,34 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import NotesPreview from './NotesPreview';
 
 class FolderId extends Component {
+
   render() {
-    console.log('folderpage')
-    console.log()
-   return (
-    <main className="App">
-        <h1>HomePage</h1>       
-    </main>
-   );
+    // console.log('folderpage', this.props.routerProps) 
+    // console.log(this.props.notes)
+    // elements={this.props.notes.find(note => note.folderId === folders.match.params.id)}
+    //folderId = //find folder id
+    // console.log(this.props.notes.folderId)
+    const folderId = this.props.routerProps.match.params.FolderId;
+    // console.log(this.props.routerProps)
+    const notes = this.props.notes.filter(note => note.folderId === folderId)
+    // console.log(notes)
+    return (
+      <main className="App">
+        <div className="title">
+          <Link to={`/HomePage`}><h1>Noteful</h1></Link>
+        </div>
+        {notes.map((item, key) => {
+          // console.log(item.id, key)
+          return <NotesPreview key={item.id} id={item.id} itemName={item.name} itemcontent={item.content}></NotesPreview>
+        }
+          //note component should be here
+        )}
+        {/* <NavLink to={`/note/${noteId}`} className="noteItem">{notes}</NavLink> */}
+      </main>
+    );
   }
 }
 
 export default FolderId;
-
--- src/FolderItem.js
+++ src/FolderItem.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
 class Item extends Component {
-  click() {
-    console.log(this.props.item.id);
-    this.props.filterNote(this.props.item.id);
-  }
+  // click() {
+  //   console.log(this.props.item.id);
+  //   this.props.filterNote(this.props.item.id);
+  // }
+  folderId = this.props.item.id;
   render() {
     const folderId = this.props.item.id;
-    return (      
+    // console.log(folderId);
+    return (
       <div>
-        <NavLink to={`/folder/${folderId}`} className="noteItem">{this.props.item.name}</NavLink>
+        {/* <NavLink to={`/folder/${folderId}`} className="noteItem">{this.props.item.name}</NavLink> */}
+        <NavLink to={`/folder/${folderId}`} className="folderItem">{this.props.item.name}</NavLink>
       </div>
     )
   }
